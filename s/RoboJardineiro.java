ac3dff6179d37375eb40a9d69caf1b6559568a8e
bruno.andrade <bruno.andrade@fastsolucoes.com.br>
2022-05-22 23:14:37 -0300

    C:/Program Files/Git/refactoring - refatoração da classe que violava o SRP

---
+++ s/RoboJardineiro.java
@@ -0,0 +1,20 @@
+package s;
+
+/**
+ * SRP refactoring
+ * Classe segue o Princípio da Responsabilidade Única
+ */
+public class RoboJardineiro {
+
+    private String planta;
+    private String adubo;
+    
+    public RoboJardineiro(String planta, String adubo) {
+        this.planta = planta;
+        this.adubo = adubo;
+    }
+
+    public void plantar() {
+        /** implementação */
+    }
+}
